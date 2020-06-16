de4f4edba4af668982297266cd444f670b071a16
Trishka <snowflak@yandex.ru>
2020-06-16 21:05:09 +0300

    add module calculate, add user input/form data for the POST request

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,27 +1,37 @@
 const modals = () =>{
-    function bindModal (triggerSelector, modalSelector, closeSelector) {
+    function bindModal (triggerSelector, modalSelector, closeSelector, closeClickOverflay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
               popup = document.querySelector(modalSelector),
-              btnClose = document.querySelector(closeSelector);
+              btnClose = document.querySelector(closeSelector),
+              windows = document.querySelectorAll('[data-modal]');
     
         trigger.forEach(trigger => {
             trigger.addEventListener('click', (e) => {
                 if(e.target){
                 e.preventDefault();
                 }
+                windows.forEach(item =>{
+                    item.style.display = 'none';
+                });
                 openPopup();
             });
         });
     
     
         popup.addEventListener('click', (e) =>{
-            if(e.target === popup){
+            if(e.target === popup && closeClickOverflay){
+                windows.forEach(item =>{
+                    item.style.display = 'none';
+                });
                 closePopup();
             }
         });
     
         btnClose.addEventListener('click', (e) =>{
             e.preventDefault();
+            windows.forEach(item =>{
+                item.style.display = 'none';
+            });
             closePopup();
         });
     
@@ -46,7 +56,10 @@ const modals = () =>{
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
-    showModalByTime('.popup', 60000);
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+    // showModalByTime('.popup', 60000);
 };
 
 
